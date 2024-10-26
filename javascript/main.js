bc6ae74479f4f5acda2b9e34ab5f5b73f4bcd5dd
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-10-26 13:41:59 -0300

    hobbies and contacts sections added

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,17 @@
+//menu mobile
+
 const menuMobile = document.querySelector(".menu-mobile");
 const menuList = document.querySelector(".nav-list");
+const menuItems = document.querySelectorAll(".list-item a");
 
 menuMobile.addEventListener("click", () => {
     menuList.classList.toggle("nav-list-open");
 });
+
+menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        menuList.classList.remove("nav-list-open");
+    });
+});
+
+//carrossel
